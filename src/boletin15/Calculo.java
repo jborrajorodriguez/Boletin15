f53e85a058181f06b33ade0f337bf8463b5885af
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-12-12 10:10:26 +0100

    Inicio proyecto

--- src/boletin15/Calculo.java
+++ src/boletin15/Calculo.java
@@ -7,21 +7,30 @@ import javax.swing.JOptionPane;
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class Calculo {
-    private float notaEscrita1,notaEscrita2,practica,nBoletines,notaTotal;
-   
-   public void calculoDeNotas(){
-       notaEscrita1=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la primera proba escrita"));
-       notaEscrita2=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la segunda proba escrita"));
-       practica=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la proba practica"));
-       nBoletines=Float.parseFloat(JOptionPane.showInputDialog("Introduce el numero de boletines"));
-       notaTotal=(float) ((((notaEscrita1+notaEscrita2)/2)*0.4)+(practica*0.4));
-       
-       
-     
-      
-         
-       
-   }
-    
+
+    private float notaEscrita1, notaEscrita2, practica, nBoletines, nBoletinesTotal=17, notaTotal,notaBoletin;
+
+    public void calculoDeNotas() {
+        notaEscrita1=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la primera proba escrita"));
+        notaEscrita2=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la segunda proba escrita"));
+        practica=Float.parseFloat(JOptionPane.showInputDialog("Introduce la nota de la proba practica"));
+        nBoletines=Float.parseFloat(JOptionPane.showInputDialog("Introduce el numero de boletines"));
+
+        if (nBoletines>(nBoletinesTotal/0.9)) {
+            notaBoletin=2;
+        }
+        else if (nBoletines<nBoletinesTotal/0.9&&nBoletines>nBoletinesTotal/0.7) {
+            notaBoletin=1;
+        }
+        else {
+            notaBoletin=0;
+
+        }
+
+        notaTotal=(float) ((((notaEscrita1+notaEscrita2)/2)*0.4)+(practica*0.4))+notaBoletin;
+        
+        JOptionPane.showMessageDialog(null,"Nota de la avaliación = "+notaTotal);
+
+    }
 
 }
