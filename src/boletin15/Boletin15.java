d14059c147e893b8275175377ef584df34b3a20a
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-12-13 08:35:39 +0100

    Clase main

---
+++ src/boletin15/Boletin15.java
@@ -0,0 +1,14 @@
+package boletin15;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin15 {
+
+    public static void main(String[] args) {
+        
+        Calculo alumno1=new Calculo();
+        alumno1.calculoDeNotas();
+
+    }
+
+}
